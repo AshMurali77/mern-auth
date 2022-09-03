fb6b759b600457a49545cb29ac97b53204a865c7
AshMurali77 <ashmurali77@gmail.com>
2022-09-03 14:19:39 -0500

    minor changes

--- frontend/src/pages/Vision.js
+++ frontend/src/pages/Vision.js
@@ -54,9 +54,9 @@ const Vision = () => {
                   <Card.Text>
                     With records being sent through snail mail or slow digital
                     platforms, current educational document transfers can take
-                    days to process. ALO's decentralized approach aims to create
-                    an instantaneous transfer process, saving time for both
-                    students and institutions alike.
+                    weeks to process. ALO's decentralized approach aims to
+                    create an instantaneous transfer process, saving time for
+                    both students and institutions alike.
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -69,11 +69,11 @@ const Vision = () => {
                 <Card.Body className="text-center  justify-content-around">
                   <Card.Title>Labor Usage</Card.Title>
                   <Card.Text>
-                    *metric* man hours are spent yearly on the manual task of
-                    educational record handling, driving productivity down and
-                    costs up. ALO seeks to create an automated record processing
-                    system, removing the burden of validation and record
-                    handling from institutions.
+                    Thousands of man hours are spent yearly on the manual task
+                    of educational record handling, driving productivity down
+                    and costs up. ALO seeks to create an automated record
+                    processing system, removing the burden of validation and
+                    record handling from institutions.
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -117,13 +117,13 @@ const Vision = () => {
               <Carousel.Caption className=" d-flex flex-column h-100 align-items-center justify-content-center">
                 <h3>Students</h3>
                 <p className="">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Consectetur esse labore eaque eveniet. Maiores quos non, eaque
-                  rerum quia vitae. Beatae, reprehenderit inventore? Vitae animi
-                  numquam maiores aut recusandae nostrum illo consequuntur
-                  magnam dignissimos molestias dolorem ut consequatur optio
-                  minima incidunt totam accusamus iure obcaecati nisi, deserunt
-                  reiciendis mollitia odit.
+                  Students' ALO profiles become their digital twins, securely
+                  documenting their academic and professional achievements.
+                  Whether they are applying to higher educational institutions
+                  or career-defining roles, students can efficiently send their
+                  records to the organizations that need them. The end goal?
+                  Save students time and money, resulting in better academic
+                  outcomes and professional contributions.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
@@ -136,13 +136,15 @@ const Vision = () => {
               <Carousel.Caption className=" d-flex flex-column h-100 align-items-center justify-content-center">
                 <h3>Institutions</h3>
                 <p className="">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Consectetur esse labore eaque eveniet. Maiores quos non, eaque
-                  rerum quia vitae. Beatae, reprehenderit inventore? Vitae animi
-                  numquam maiores aut recusandae nostrum illo consequuntur
-                  magnam dignissimos molestias dolorem ut consequatur optio
-                  minima incidunt totam accusamus iure obcaecati nisi, deserunt
-                  reiciendis mollitia odit.
+                  The manual, hassle-ridden process of educational record
+                  processing can be transformed with the help of ALO's
+                  decentralized approach. Robust analytics capabilities allow
+                  institutions to garner a wealth of insight from their record
+                  data, meaning better evaluation of academic outcomes and
+                  professional opportunities. ALO's automatic
+                  transfer-equivalency process broadens student application
+                  opportunities and allows an informed approach to course
+                  content curation.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
@@ -155,13 +157,13 @@ const Vision = () => {
               <Carousel.Caption className=" d-flex flex-column h-100 align-items-center justify-content-center">
                 <h3>Employers</h3>
                 <p className="">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Consectetur esse labore eaque eveniet. Maiores quos non, eaque
-                  rerum quia vitae. Beatae, reprehenderit inventore? Vitae animi
-                  numquam maiores aut recusandae nostrum illo consequuntur
-                  magnam dignissimos molestias dolorem ut consequatur optio
-                  minima incidunt totam accusamus iure obcaecati nisi, deserunt
-                  reiciendis mollitia odit.
+                  Employers can use the ALO platform to connect with top
+                  candidates and target their recruitment to those with the
+                  education and skills perfect for the job. Special
+                  certifications and job-readiness metrics can be parsed through
+                  on the ALO platform, meaning more time saved for building
+                  relationships with top candidates and institutions around the
+                  globe.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
@@ -174,13 +176,12 @@ const Vision = () => {
               <Carousel.Caption className=" d-flex flex-column h-100 align-items-center justify-content-center">
                 <h3>Learning Platforms</h3>
                 <p className="">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Consectetur esse labore eaque eveniet. Maiores quos non, eaque
-                  rerum quia vitae. Beatae, reprehenderit inventore? Vitae animi
-                  numquam maiores aut recusandae nostrum illo consequuntur
-                  magnam dignissimos molestias dolorem ut consequatur optio
-                  minima incidunt totam accusamus iure obcaecati nisi, deserunt
-                  reiciendis mollitia odit.
+                  Online learning platforms can continue towards their goals of
+                  providing low cost, relevant education to students across the
+                  globe. Through ALO's course equivalency platform, learning
+                  platforms can curate course content that is transferrable to
+                  traditional institutions, greatly increasing the efficacy of
+                  digital courses for everyone.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
