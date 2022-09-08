02cf8df771e3bfed36d1b6186ce6e881f5536a4f
AshMurali77 <ashmurali77@gmail.com>
2022-09-07 22:54:14 -0500

    production commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const contactRoutes = require("./routes/contact");
-
+const path = require("path");
 //express app
 const app = express();
 
@@ -19,6 +19,21 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/users", userRoutes);
 app.use("/api/contacts", contactRoutes);
+
+//Serve frontend
+_dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 //connect to db
 mongoose
   .connect(process.env.MONGO_URI)
